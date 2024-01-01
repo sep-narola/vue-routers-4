854f0037e7638a849d1c455f0a6239751d4e9df7
sep-narola <sep@narola.email>
2024-01-01 11:38:21 +0530

    Added tailwind css

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import AppLink from "@/components/AppLink.vue";
+import "../public/main.css";
 
 createApp(App).component("AppLink", AppLink).use(router).mount("#app");
--- src/views/ProtectedView.vue
+++ src/views/ProtectedView.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <div><h1>Protected</h1></div>
+    <div>
+      <h1>Protected</h1>
+    </div>
     Greetings, {{ username }},
     <a style="cursor: pointer" @click="logout()">Logout</a>
     <br />
     <router-link :to="{ name: 'invoices' }">
       <button>Invoices</button>
     </router-link>
+    <br />
   </div>
 </template>
 
